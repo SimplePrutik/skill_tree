f1a8dd64ad16cb0cbbb3cd0b50df615ab22942d8
SimplePrutik <dimodaktil@yandex.ru>
2020-08-19 00:10:30 +0300

    initial scene

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Skill", menuName = "Skill", order = 51)]
+public class Skill : ScriptableObject
+{
+    [SerializeField]
+    private string skillName;
+    [SerializeField]
+    private int cost;
+    
+}
